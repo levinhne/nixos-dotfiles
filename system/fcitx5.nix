2aebed7ebf366bf06f1019dd32725890f752b383
levinhne <levinhne@example.com>
2026-03-11 16:34:58 +0700

    refactor fcitx5 configuration for input method and addons

--- system/fcitx5.nix
+++ system/fcitx5.nix
@@ -5,10 +5,8 @@
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-gtk
       qt6Packages.fcitx5-unikey
-      qt6Packages.fcitx5-configtool
+      fcitx5-gtk
     ];
-    fcitx5.waylandFrontend = true;
   };
 }
