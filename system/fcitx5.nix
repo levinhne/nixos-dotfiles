739f1687e7571dd56bda0c55e56feeb257614dec
levinhne <levinhne@example.com>
2026-03-11 16:13:23 +0700

    refactor code

--- system/fcitx5.nix
+++ system/fcitx5.nix
@@ -5,12 +5,10 @@
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
-      qt6Packages.fcitx5-unikey
       fcitx5-gtk
+      qt6Packages.fcitx5-unikey
+      qt6Packages.fcitx5-configtool
     ];
+    fcitx5.waylandFrontend = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    qt6Packages.fcitx5-configtool
-  ];
 }
